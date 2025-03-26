57907e6ab619d0514bbf641add89832b801a755c
AI4science-ym <2417854391@qq.com>
2025-03-26 15:21:37 +0800

    Add files via upload

---
+++ cell_dict.py
@@ -0,0 +1,68 @@
+
+
+Skin_full={0: 'B cells', 1: 'Doublet', 2: 'Endothelial cells', 3: 'Epithelial cells_Cancer cells', 4: 'Fibroblasts', 5: 'Hair follicle cells', 6: 'Keratinocytes', 7: 'Langerhans cells', 8: 'Mast cells', 9: 'Melanocytes', 10: 'Melanocytes_Cancer cells', 11: 'Mononuclear phagocytes', 12: 'Mural cells', 13: 'Plasma cells', 14: 'Plasmacytoid dendritic cells', 15: 'Proliferating cells', 16: 'Schwann cells', 17: 'Sweat gland cells', 18: 'T cells', 19: 'T cells_Cancer cells', 20: 'Unassigned'}
+Brain_full={0: 'Acinar cells', 1: 'Alveolar type I cells', 2: 'Alveolar type II cells', 3: 'Astrocytes', 4: 'B cells', 5: 'B cells_Cancer cells', 6: 'Ciliated cells', 7: 'Club cells', 8: 'Doublet', 9: 'Ductal cells', 10: 'Endothelial cells', 11: 'Ependymal cells', 12: 'Epithelial cells_Cancer cells', 13: 'Fibroblasts', 14: 'Glial cells_Cancer cells', 15: 'Mast cells', 16: 'Melanocytes_Cancer cells', 17: 'Microglial cells', 18: 'Mononuclear phagocytes', 19: 'Mural cells', 20: 'Myocytes_Cancer cells', 21: 'Neurons', 22: 'Neurons_Cancer cells', 23: 'Oligodendrocyte precursor cells', 24: 'Oligodendrocytes', 25: 'Pancreatic beta cells', 26: 'Plasma cells', 27: 'Plasmacytoid dendritic cells', 28: 'Proliferating cells', 29: 'Proliferating lymphocytes', 30: 'T cells', 31: 'Unassigned'}
+Liver_full={0: 'B cells', 1: 'Cholangiocytes', 2: 'Doublet', 3: 'Endothelial cells', 4: 'Epithelial cells_Cancer cells', 5: 'Erythrocytes', 6: 'Fibroblasts', 7: 'Granulocytes-monocyte progenitor cells', 8: 'Hematopoietic stem cells', 9: 'Hepatic stellate cells', 10: 'Hepatocytes', 11: 'Hepatocytes_Cancer cells', 12: 'Innate lymphoid cells', 13: 'Kupffer cells', 14: 'Mast cells', 15: 'Mononuclear phagocytes', 16: 'Mural cells', 17: 'Neutrophils', 18: 'Plasma cells', 19: 'Plasmacytoid dendritic cells', 20: 'Platelets', 21: 'Proliferating cells', 22: 'Stromal cells', 23: 'T cells'}
+Kidney_full={0: 'B cells', 1: 'Collecting duct intercalated cells', 2: 'Collecting duct principal cells', 3: 'Distal tubule cells', 4: 'Doublet', 5: 'Endothelial cells', 6: 'Epithelial cells', 7: 'Epithelial cells_Cancer cells', 8: 'Erythrocytes', 9: 'Fibroblasts', 10: 'Innate lymphoid cells', 11: 'Intercalated cells', 12: 'Interstitial cells', 13: 'Kidney loop of Henle epithelial cells', 14: 'Mast cells', 15: 'Megakaryocytes', 16: 'Mesangial cells', 17: 'Mesenchymal cells_Cancer cells', 18: 'Mesenchymal stem cells', 19: 'Mononuclear phagocytes', 20: 'Mural cells', 21: 'Nephron progenitor cells', 22: 'Parietal epithelial cells', 23: 'Plasma cells', 24: 'Plasmacytoid dendritic cells', 25: 'Podocytes', 26: 'Pro-B cells', 27: 'Proliferating cells', 28: 'Proximal tubule cells', 29: 'T cells', 30: 'Unassigned', 31: 'Ureteric bud cells'}
+Skin_sub={0: 'Arterial endothelial cells', 1: 'B cells', 2: 'CD4+ effector T cells', 3: 'CD4+ exhausted T cells', 4: 'CD4+ naive T cells', 5: 'CD8+ effector T cells', 6: 'CD8+ effector memory T cells', 7: 'CD8+ exhausted T cells', 8: 'CD8+ memory T cells', 9: 'CD8+ mucosal-associated invariant T cells', 10: 'CD8+ naive T cells', 11: 'CD8+ tissue-resident memory T cells', 12: 'Capillary endothelial cells', 13: 'Conventional dendritic cells', 14: 'Doublet', 15: 'Endothelial cells', 16: 'Endothelial tip cells', 17: 'Epithelial cells_Cancer cells', 18: 'Exhausted T cells', 19: 'Fibroblasts', 20: 'Gamma delta T cells', 21: 'Hair follicle cells', 22: 'Keratinocytes', 23: 'Langerhans cells', 24: 'Lymphatic endothelial cells', 25: 'Macrophages', 26: 'Mast cells', 27: 'Melanocytes', 28: 'Melanocytes_Cancer cells', 29: 'Memory B cells', 30: 'Monocytes', 31: 'Mural cells', 32: 'Naive B cells', 33: 'Naive T cells', 34: 'Natural killer cells', 35: 'Plasma cells', 36: 'Plasmacytoid dendritic cells', 37: 'Proliferating T cells', 38: 'Proliferating cells', 39: 'Regulatory T cells', 40: 'Schwann cells', 41: 'Sweat gland cells', 42: 'T cells_Cancer cells', 43: 'T helper cells', 44: 'T-helper 2 cells', 45: 'Unassigned', 46: 'Venous endothelial cells'}
+Brain_sub={0: 'Acinar cells', 1: 'Alveolar type I cells', 2: 'Alveolar type II cells', 3: 'Astrocytes', 4: 'B cells', 5: 'B cells_Cancer cells', 6: 'CD4+ effector T cells', 7: 'CD4+ effector memory T cells', 8: 'CD4+ naive T cells', 9: 'CD8+ effector T cells', 10: 'CD8+ effector memory T cells', 11: 'CD8+ exhausted T cells', 12: 'CD8+ memory T cells', 13: 'CD8+ naive T cells', 14: 'CD8+ tissue-resident memory T cells', 15: 'Capillary endothelial cells', 16: 'Ciliated cells', 17: 'Club cells', 18: 'Conventional dendritic cells', 19: 'Doublet', 20: 'Ductal cells', 21: 'Endothelial cells', 22: 'Ependymal cells', 23: 'Epithelial cells_Cancer cells', 24: 'Exhausted T cells', 25: 'Fibroblasts', 26: 'Follicular helper T cells', 27: 'Glial cells_Cancer cells', 28: 'Macrophages', 29: 'Mast cells', 30: 'Melanocytes_Cancer cells', 31: 'Microglial cells', 32: 'Monocytes', 33: 'Mural cells', 34: 'Myocytes_Cancer cells', 35: 'Naive T cells', 36: 'Natural killer cells', 37: 'Neurons', 38: 'Neurons_Cancer cells', 39: 'Oligodendrocyte precursor cells', 40: 'Oligodendrocytes', 41: 'Pancreatic beta cells', 42: 'Plasma cells', 43: 'Plasmacytoid dendritic cells', 44: 'Proliferating T cells', 45: 'Proliferating cells', 46: 'Proliferating lymphocytes', 47: 'Proliferating macrophages', 48: 'Regulatory T cells', 49: 'T helper cells', 50: 'Unassigned', 51: 'Venous endothelial cells'}
+Liver_sub={0: 'Arterial endothelial cells', 1: 'B cells', 2: 'CD4+ effector T cells', 3: 'CD4+ memory T cells', 4: 'CD4+ naive T cells', 5: 'CD8+ effector T cells', 6: 'CD8+ effector memory T cells', 7: 'CD8+ exhausted T cells', 8: 'CD8+ mucosal-associated invariant T cells', 9: 'CD8+ naive T cells', 10: 'CD8+ tissue-resident memory T cells', 11: 'Capillary endothelial cells', 12: 'Cholangiocytes', 13: 'Conventional dendritic cells', 14: 'Doublet', 15: 'Endothelial cells', 16: 'Endothelial tip cells', 17: 'Epithelial cells_Cancer cells', 18: 'Erythrocytes', 19: 'Fibroblasts', 20: 'Follicular helper T cells', 21: 'Gamma delta T cells', 22: 'Granulocytes-monocyte progenitor cells', 23: 'Hematopoietic stem cells', 24: 'Hepatic stellate cells', 25: 'Hepatocytes', 26: 'Hepatocytes_Cancer cells', 27: 'Innate lymphoid cells', 28: 'Kupffer cells', 29: 'Lymphatic endothelial cells', 30: 'Macrophages', 31: 'Mast cells', 32: 'Memory B cells', 33: 'Monocytes', 34: 'Mural cells', 35: 'NK T cells', 36: 'Naive B cells', 37: 'Natural killer cells', 38: 'Neutrophils', 39: 'Plasma cells', 40: 'Plasmacytoid dendritic cells', 41: 'Platelets', 42: 'Proliferating CD8+ effector T cells', 43: 'Proliferating cells', 44: 'Regulatory T cells', 45: 'Stromal cells', 46: 'T helper cells', 47: 'Venous endothelial cells'}
+Kidney_sub={0: 'Arterial endothelial cells', 1: 'B cells', 2: 'CD4+ naive T cells', 3: 'CD4+ tissue-resident memory T cells', 4: 'CD8+ effector T cells', 5: 'CD8+ effector memory T cells', 6: 'CD8+ exhausted T cells', 7: 'CD8+ memory T cells', 8: 'CD8+ mucosal-associated invariant T cells', 9: 'CD8+ naive T cells', 10: 'Capillary endothelial cells', 11: 'Collecting duct intercalated cells', 12: 'Collecting duct principal cells', 13: 'Conventional dendritic cells', 14: 'Distal tubule cells', 15: 'Doublet', 16: 'Effector T cells', 17: 'Endothelial cells', 18: 'Endothelial tip cells', 19: 'Epithelial cells', 20: 'Epithelial cells_Cancer cells', 21: 'Erythrocytes', 22: 'Fibroblasts', 23: 'Follicular helper T cells', 24: 'Glomerular endothelial cells', 25: 'Innate lymphoid cells', 26: 'Intercalated cells', 27: 'Interstitial cells', 28: 'Kidney loop of Henle epithelial cells', 29: 'Lymphatic endothelial cells', 30: 'Macrophages', 31: 'Mast cells', 32: 'Megakaryocytes', 33: 'Memory B cells', 34: 'Memory T cells', 35: 'Mesangial cells', 36: 'Mesenchymal cells_Cancer cells', 37: 'Mesenchymal stem cells', 38: 'Monocytes', 39: 'Mural cells', 40: 'Naive B cells', 41: 'Natural killer cells', 42: 'Nephron progenitor cells', 43: 'Parietal epithelial cells', 44: 'Plasma cells', 45: 'Plasmacytoid dendritic cells', 46: 'Podocytes', 47: 'Pro-B cells', 48: 'Proliferating CD8+ effector T cells', 49: 'Proliferating T cells', 50: 'Proliferating cells', 51: 'Proximal tubule cells', 52: 'Regulatory T cells', 53: 'T cells', 54: 'T helper cells', 55: 'T-helper 2 cells', 56: 'Unassigned', 57: 'Ureteric bud cells', 58: 'Venous endothelial cells'}
+tissue_list=["Brain","Liver","Kidney","Skin"]
+full_dict={"Skin": Skin_full, "Brain": Brain_full, "Kidney": Kidney_full, "Liver": Liver_full}
+sub_dict={"Skin": Skin_sub, "Brain": Brain_sub, "Kidney": Kidney_sub, "Liver": Liver_sub}
+def choode_dict(args):
+    for i in tissue_list:
+        if "full" in args.celltype_name and i in args.tissue_name:
+            return full_dict[i]
+        if "sub" in args.celltype_name and i in args.tissue_name:
+            return sub_dict[i]
+    else:
+        raise ValueError("Invalid data path")
+def choose_mode(args):
+    if args.model_path==None and args.tissue_name!= None:
+        if "full" in args.celltype_name:
+            celltype_name="full"
+        if "sub" in args.celltype_name:
+            celltype_name="sub"
+        model_path = "/gpfstest/tako/singlecell/dataset/ylm/checkpoint/"+args.tissue_name+"/"+celltype_name
+        return model_path
+    else:
+        return args.model_path
+import pandas as pd
+def choose_tissue(args):
+    tissue_name=None
+    if args.tissue_name==None:
+        batch2=pd.read_excel(r'/gpfstest/tako/singlecell/dataset/zeng2/synecosys_datasets/华智第二批数据600.xlsx')
+        batch1=pd.read_csv(r'/gpfstest/tako/singlecell/dataset/zeng/华智第一批意向数据集450个_修改.xls', sep='\t', encoding='ISO-8859-1')
+        root1="dataset_process_batch1/"
+        root2="dataset_process_batch_1_2/"
+        for j in tissue_list:
+            if j in args.data_path:
+                tissue_name= j
+        if  tissue_name==None :
+            if "batch_1_2" in args.data_path:
+                data_path=args.data_path.split(root2)
+                data_path=data_path[1]
+                for i in range(len(batch2.index)): 
+                    if  batch2.rds_id[i] in data_path:
+                        tissue_name= batch2.Organ[i]
+            else:
+                data_path=args.data_path.split(root1)
+                data_path=data_path[1]
+                for i in range(len(batch1.index)): 
+                    if  batch1.rds_id[i] in data_path:
+                        tissue_name= batch1.Organ[i]
+    else:
+        tissue_name=args.tissue_name
+    if "full" in args.data_path:
+        celltype_name="annot_full"
+    elif "sub" in args.data_path :
+        celltype_name="annot_sub"
+    else :
+        celltype_name=args.celltype_name
+    return tissue_name,celltype_name    
+     
+
+
+
